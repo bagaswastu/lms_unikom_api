e7f323796bffa7c064ee21bb2f5314d85d855158
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2021-02-27 10:49:17 +0700

    Fix get file instance

--- app/routes/instance.py
+++ app/routes/instance.py
@@ -1,6 +1,7 @@
 import requests
-from fastapi.openapi.models import Response
+from fastapi import HTTPException
 from fastapi.routing import APIRouter
+from starlette.responses import Response
 
 from app.constant import BASE_URL
 
@@ -8,10 +9,13 @@ router = APIRouter()
 
 
 @router.get('/file/{id}', description="Untuk download file materi dari mata kuliah")
-async def get_file(id, cookies: str):
-    cookies = {'MoodleSession', cookies}
-    url = BASE_URL + '/mod/resource/view.php?id=' + id
-    res = requests.get(url, cookies={'MoodleSession': cookies}, stream=True)
-    if res.headers.get('Content-Disposition'):
-        file = res.content
-        return Response(file, headers=dict(res.headers))
+async def get_file(id, cookies):
+    try:
+        cookies = {'MoodleSession': cookies}
+        url = BASE_URL + '/mod/resource/view.php?id=' + id
+        res = requests.get(url, cookies=cookies, stream=True)
+        if res.headers.get('Content-Disposition'):
+            file = res.content
+            return Response(file, headers=dict(res.headers))
+    except requests.exceptions.TooManyRedirects:
+        raise HTTPException(status_code=401, detail={'error_message': 'Autentikasi salah'})
